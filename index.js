de787181a7edb0e50ffbb40020f43d16ef4ba25b
renzjohn06 <renzd2719@gmail.com>
2024-06-07 21:18:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
+const btnLike1 = document.getElementById("btnLike1")
+const count1 = document.getElementById("count1")
 
+function countLike1() {
+  let likeCounts = parseInt(count1.value) + 1
+  count1.textContent = likeCounts.toString()
+}
+
+btnLike1.addEventListener("click", countLike1)
+
+const btnLike2 = document.getElementById("btnLike2")
+const count2 = document.getElementById("count2")
+
+function countLike2() {
+  let likeCounts = parseInt(count2.value) + 1
+  count2.textContent = likeCounts.toString()
+}
+
+btnLike2.addEventListener("click", countLike2)
+
+const btnDislike1 = document.getElementById("btnDislike1")
+const count3 = document.getElementById("count3")
+
+function countDislike1() {
+  let DislikeCounts = parseInt(count3.value) + 1
+  count3.textContent = DislikeCounts.toString()
+}
+
+btnDislike1.addEventListener("click", countDislike1)
+
+const btnDislike2 = document.getElementById("btnDislike2")
+const count4 = document.getElementById("count4")
+
+function countDislike2() {
+  let DislikeCounts = parseInt(count4.value) + 1
+  count4.textContent = DislikeCounts.toString()
+}
+
+btnDislike2.addEventListener("click", countDislike2)
